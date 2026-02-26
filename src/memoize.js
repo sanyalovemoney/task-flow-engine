ed1cc9bd2f9ad420fc2a2f5e63584a588b43d91c
sanyalovemoney <cashamashuta@gmail.com>
2026-02-26 17:54:09 +0200

    add warning

--- src/memoize.js
+++ src/memoize.js
@@ -4,6 +4,10 @@ export function memoizeById(fn) {
     return function(...args) {
         const id = (args[0] && typeof args[0] === 'object' && args[0].id) || JSON.stringify(args);
 
+        if (id === undefined) {
+            throw new Error("Аргумент повинен бути об'єктом з властивістю 'id' або серіалізованим значенням");
+        }
+        
         if (cache.has(id)) {
             return cache.get(id);
         }
