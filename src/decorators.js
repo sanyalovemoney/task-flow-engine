36d34761137e8851b79bcfb351fe43142720a8ee
sanyalovemoney <cashamashuta@gmail.com>
2026-02-27 16:46:11 +0200

    fix decorator

--- src/decorators.js
+++ src/decorators.js
@@ -1,14 +1,16 @@
 export function logExecution(fn) {
-  return function(...args) {
+  const loggedFn = function(...args) {
     const start = performance.now();
     const result = fn.apply(this, args);
     const end = performance.now();
 
-console.group('log for: ${fn.name}');
-console.log("Input data:", args);
-console.log(`Execution time: ${(end - start).toFixed(3)}ms`);
-console.groupEnd();
+    console.group(`log for: ${fn.name}`);
+    console.log("Input data:", args);
+    console.log(`Execution time: ${(end - start).toFixed(3)}ms`);
+    console.groupEnd();
 
     return result;
   };
+  Object.defineProperty(loggedFn, 'name', { value: fn.name || 'unknown' });
+  return loggedFn;
 }
