1687fd43c4629dbd36a77246ef93b428aacc2071
sanyalovemoney <cashamashuta@gmail.com>
2026-03-02 18:38:53 +0200

    add asyncMapPromise

--- src/async-utils.js
+++ src/async-utils.js
@@ -10,3 +10,13 @@ export function asyncMapCallback(array, callback, onDone) {
     });
   });
 }
+
+export function asyncMapPromise(array, asyncFn) {
+  return new Promise((resolve, reject) => {
+    const promises = array.map(async (item) => {
+      return await asyncFn(item);
+    });
+
+    Promise.all(promises).then(resolve).catch(reject);
+  });
+}
