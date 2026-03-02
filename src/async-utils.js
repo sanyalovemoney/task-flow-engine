e96b40354c6549c0baa479f6eb6305a6bd811937
sanyalovemoney <cashamashuta@gmail.com>
2026-03-02 18:42:22 +0200

    integration AbortSignal

--- src/async-utils.js
+++ src/async-utils.js
@@ -11,11 +11,15 @@ export function asyncMapCallback(array, callback, onDone) {
   });
 }
 
-export function asyncMapPromise(array, asyncFn) {
+export function asyncMapPromise(array, asyncFn, signal) {
   return new Promise((resolve, reject) => {
+     if (signal?.aborted) return reject(new Error("Aborted"));
     const promises = array.map(async (item) => {
+    if (signal?.aborted) throw new Error("Aborted");
       return await asyncFn(item);
     });
+
+    signal?.addEventListener('abort', () => reject(new Error("Aborted")));
 
     Promise.all(promises).then(resolve).catch(reject);
   });
