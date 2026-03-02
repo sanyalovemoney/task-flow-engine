45d06aea1e4ee73cecb2c673232f8524c8ec824a
sanyalovemoney <cashamashuta@gmail.com>
2026-03-02 14:11:12 +0200

    add enqueue

--- src/queue.js
+++ src/queue.js
@@ -3,4 +3,13 @@ export class BiDirectionalPriorityQueue {
     this.items = [];
     this.counter = 0; 
   }
+
+enqueue(item, priority) {
+    this.items.push({
+      item,
+      priority,
+      order: this.counter++ 
+    });
+    console.log(`[Queue] Додано: ${item}, пріоритет: ${priority}`);
+  }
 }
