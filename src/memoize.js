083decd499590bb2ca27a7ec7c47561aaa7849d9
sanyalovemoney <cashamashuta@gmail.com>
2026-03-06 20:27:43 +0200

    last version memoize

--- src/memoize.js
+++ src/memoize.js
@@ -19,4 +19,34 @@ export function memoizeAdvanced(fn, { maxSize = Infinity, strategy = 'LRU', ttl 
         if (strategy === 'LFU') key = [...meta].reduce((a, b) => a[1].f < b[1].f ? a : b)[0];
         if (key) { cache.delete(key); meta.delete(key); }
     };
-};
+ const memo = function(...args) {
+        const key = JSON.stringify(args), now = Date.now();
+        
+        if (cache.has(key)) {
+            const m = meta.get(key);
+            if (ttl && now - m.t > ttl) {
+                cache.delete(key);
+                meta.delete(key);
+            } else {
+                if (strategy === 'LRU') { 
+                    const val = cache.get(key);
+                    cache.delete(key); 
+                    cache.set(key, val); 
+                }
+                m.f++;
+                return cache.get(key);
+            }
+        }
+
+        if (cache.size >= maxSize) evict();
+        const res = fn.apply(this, args);
+        cache.set(key, res);
+        meta.set(key, { t: now, f: 1 });
+        return res;
+    };
+
+    memo.getCacheSize = () => cache.size;
+    memo.clear = () => { cache.clear(); meta.clear(); };
+    
+    return memo;
+}
